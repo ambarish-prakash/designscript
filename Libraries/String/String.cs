276cb4a38110ab22c5108bddeafadef6a6b28118
ambarish-prakash <t_praka@SINSGH93407YY.ads.autodesk.com>
2013-09-18 17:05:23 +0800

    Extra functions

--- Libraries/String/String.cs
+++ Libraries/String/String.cs
@@ -32,27 +32,17 @@ namespace String
         {
             string[] strArray = { delimiter };
             string[] resultArray = str.Split(strArray, System.StringSplitOptions.RemoveEmptyEntries);
-            List<string> resultList = new List<string>();
-            foreach (string s in resultArray)
-            {
-                resultList.Add(s);
-            }
-            return resultList;
+            return resultArray.ToList();
         }
 
-        public static string JoinStrings(List<string> str, string delimiter)
+        public static string JoinStrings(string[] str, string delimiter)
         {
-            string result = "";
-            int i = 0;
-            for (; i < str.Count - 1; i++)
-            {
-                result = result + str[i] + delimiter;
-            }
-            result = result + str[i];
-            return result;
+            if (str == null)
+                return "";
+            return string.Join(delimiter, str);
         }
 
-        public static string JoinStrings(List<string> str)
+        public static string JoinStrings(string[] str)
         {
             return JoinStrings(str, "");
         }
@@ -61,5 +51,189 @@ namespace String
         {
             return str.Substring(start, length);
         }
+
+        public static string NumberToString(double num)
+        {
+            return num.ToString();
+        }
+
+        public static List<double> NumberSequence(double start, int amount, double step)
+        {
+            double number = start;
+            List<double> result = new List<double>();
+            for (int i = 0; i < amount; i++)
+            {
+                result.Add(number);
+                number += step;
+            }
+            return result;
+        }
+
+        public static List<object> ShiftList(List<object> list, int amount)
+        {
+            if (amount == 0)
+                return list;
+
+            if (amount < 0)
+            {
+                return list.Skip(-amount).Concat(list.Take(-amount)).ToList();
+            }
+
+            int len = list.Count;
+            return list.Skip(len - amount).Concat(list.Take(len - amount)).ToList();
+        }
+
+        public static List<object> SliceList(List<object> list, int start, int count)
+        {
+            return list.Skip(start).Take(count).ToList();
+        }
+
+        public static List<List<object>> Slice(List<object> list, int n)
+        {
+            List<List<object>> result = new List<List<object>>();
+            List<object> subList = new List<object>();
+            int count=0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                count++;
+                subList.Add(list.ElementAt(i));
+
+                if (count == n)
+                {
+                    result.Add(subList);
+                    count = 0;
+                    subList = new List<object>();
+                }
+            }
+            if (subList.Count != 0)
+                result.Add(subList);
+            
+            return result;
+        }
+
+        public static List<object> DropList(List<object> list, int amount)
+        {
+            return list.Skip(amount).ToList();
+        }
+
+        public static List<object> RemoveEveryNth(List<object> list, int n, int offset)
+        {
+            return list.Skip(offset).Where((_, i) => (i + 1) % n != 0).ToList();
+        }
+
+        public static List<List<object>> DiagonalLeftList(List<object> list, int n)
+        {
+            List<List<object>> result = new List<List<object>>();
+            if (n > list.Count)
+            {
+                result.Add(list);
+                return result;
+            }
+            List<object> currList = new List<object>();
+            List<int> startIndices = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                startIndices.Add(i);
+            }
+
+            for (int i = n - 1 + n; i < list.Count(); i += n)
+            {
+                startIndices.Add(i);
+            }
+
+            foreach (int start in startIndices)
+            {
+                int index = start;
+
+                while (index < list.Count)
+                {
+                    var currentRow = (int)Math.Ceiling((index + 1) / (double)n);
+                    currList.Add(list.ElementAt(index));
+                    index += n - 1;
+
+                    //ensure we are skipping a row to get the next index
+                    var nextRow = (int)Math.Ceiling((index + 1) / (double)n);
+                    if (nextRow > currentRow + 1 || nextRow == currentRow)
+                        break;
+                }
+                result.Add(currList);
+                currList = new List<object>();
+            }
+
+            if (currList.Count!=0)
+            {
+                result.Add(currList);
+            }
+
+            return result;
+        }
+
+
+        public static List<List<object>> DiagonalRightList(List<object> list, int n)
+        {
+            List<List<object>> result = new List<List<object>>();
+            if (n > list.Count)
+            {
+                result.Add(list);
+                return result;
+            }
+            List<object> currList = new List<object>();
+            List<int> startIndices = new List<int>();
+
+            for (int i = n; i < list.Count(); i += n)
+            {
+                startIndices.Add(i);
+            }
+
+            startIndices.Reverse();
+
+            for (int i = 0; i < n; i++)
+            {
+                startIndices.Add(i);
+            }
+
+            foreach (int start in startIndices)
+            {
+                int index = start;
+
+                while (index < list.Count)
+                {
+                    var currentRow = (int)Math.Ceiling((index + 1) / (double)n);
+                    currList.Add(list.ElementAt(index));
+                    index += n + 1;
+
+                    //ensure we are skipping a row to get the next index
+                    var nextRow = (int)Math.Ceiling((index + 1) / (double)n);
+                    if (nextRow > currentRow + 1 || nextRow == currentRow)
+                        break;
+                }
+                result.Add(currList);
+                currList = new List<object>();
+            }
+
+            if (currList.Count != 0)
+            {
+                result.Add(currList);
+            }
+
+            return result;
+        }
+
+        public static double RandomSeed(int seed)
+        {
+            System.Random rand = new System.Random(seed);
+            return rand.NextDouble();
+        }
+
+        public static List<object> Repeat(object item, int count)
+        {
+            List<object> result = new List<object>();
+            if (count < 0)
+                return result;
+            result = Enumerable.Repeat(item, count).ToList();
+            return result;
+        }
+
     }
 }
